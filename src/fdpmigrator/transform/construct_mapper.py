a67c59ccb89206a252348a687f66ec00a9bd207d
KarolisCremers <karolis_cremers@live.nl>
2026-09-18 13:40:08 +0200

    use dcat types instead of indiviual URI's this allows for the sparql query targets to be more generic

--- src/fdpmigrator/transform/construct_mapper.py
+++ src/fdpmigrator/transform/construct_mapper.py
@@ -1,8 +1,8 @@
-from rdflib import Graph
+from rdflib import RDF, Graph
 import yaml
 from pathlib import Path
 
-query_targets = yaml.safe_load(open("config/query_targets.yaml"))["query_targets"]
+query_targets = yaml.safe_load(open("config/query_targets_lls.yaml"))["query_targets"]
 query_folder = Path("SPARQL")
 
 class ConstructMapper:
@@ -20,7 +20,7 @@ class ConstructMapper:
         """
         Applies the SPARQL CONSTRUCT query to the given RDF graph and returns the resulting graph.
         If no CONSTRUCT query is provided, it will attempt to determine the appropriate query based on the subjects in the graph.
-        This is done by checking if any of the subjects in the graph match the URIs specified in the query_targets dictionary.
+        This is done by checking if any of the subjects in the graph match dcat types specified in the query_targets dictionary. If a match is found, the corresponding SPARQL query is loaded and applied to the graph.
         The key in the query_targets dictionary corresponds to the name of the SPARQL query file (without the .rq extension) that should be used for the CONSTRUCT operation.
         The value in the query_targets dictionary is a list of URIs that are associated with that specific query.
         If a match is found, the corresponding SPARQL query is loaded from the SPARQL folder and applied to the graph. If no match is found, the original graph is returned unchanged.
@@ -29,8 +29,9 @@ class ConstructMapper:
         if self.construct_query is None:
             subjects = self.graph.subjects()
             for subject in subjects:
-                for query_name, uris in self.query_targets.items():
-                    if str(subject) in uris:
+                subjectypes = self.graph.value(subject=subject, predicate=RDF.type, object=None)
+                for query_name, types in self.query_targets.items():
+                    if str(subjectypes) in types:
                         self.construct_query = self.get_query(query_name)
                         break
                 if self.construct_query is not None:

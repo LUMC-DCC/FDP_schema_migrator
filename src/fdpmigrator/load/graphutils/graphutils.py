4e7557b2252cfd8411127c22116eb217b8853560
KarolisCremers <karolis_cremers@live.nl>
2026-09-18 16:52:31 +0200

    Add documentation and TODO

--- src/fdpmigrator/load/graphutils/graphutils.py
+++ src/fdpmigrator/load/graphutils/graphutils.py
@@ -12,12 +12,16 @@ class GraphUtils(BaseGraphUtils):
         super().__init__()
 
     def add_rdfs_reasoning(self, graph: Graph) -> Graph:
-        """Add RDFS reasoning to the graph."""
+        """Add RDFS reasoning to the graph. This adds dcat:Resource
+        to all resource classes."""
         DeductiveClosure(RDFS_Semantics).expand(graph)
         return(graph)
 
     def get_main_resource(self, graph: Graph) -> URIRef:
-        """Get the main resource of the graph."""
+        """Get the main resource of the graph. This is done through the 
+        finding the subject that is described as dcat:Resource"""
+        #TODO If in the future we would want to migrate distributions it would have to
+        # be manually added, as dcat:Distribution is not a dcat:Distribution
         result = graph.query("""PREFIX dcat: <http://www.w3.org/ns/dcat#>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         SELECT DISTINCT ?s WHERE {

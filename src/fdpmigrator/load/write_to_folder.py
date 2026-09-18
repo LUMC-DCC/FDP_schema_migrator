54202e6e3276baf28c9c5a761ce6b681088154b8
KarolisCremers <karolis_cremers@live.nl>
2026-09-18 17:05:16 +0200

    Reworked resource subject identification system, using inference to add dcat:resource to the subject triples by loading in the dcat vocabulary

--- src/fdpmigrator/load/write_to_folder.py
+++ src/fdpmigrator/load/write_to_folder.py
@@ -3,30 +3,42 @@ It follows the standard FDP structure and creates subfolders for each resource t
 The function takes a list of RDF graphs and a base path as input, and writes each graph to a separate TTL file in the appropriate subfolder based on its resource type and parent resource."""
 
 import os
-from typing import List
+
 from rdflib import Graph
-from fdpmigrator.load.graphutils.graphutils import graphutils
 
+from fdpmigrator.load.graphutils.graphutils import GraphUtils
 
-def write_to_folder(graphs: List[Graph], base_path: str):
+utils = GraphUtils()
+
+def identify_main_resource(graph: Graph, context: str) -> str:
+    """loads in the context from the config folder,
+    merges it with the graph, and identifies the main resource of the graph based on the context.
+    """
+    with open(context, "r", encoding="utf-8") as f:
+        context_graph = Graph().parse(f, format="turtle")
+        reasonable_graph = utils.add_rdfs_reasoning(graph + context_graph)
+    return utils.get_main_resource(reasonable_graph)
+
+def write_to_folder(graphs: list[Graph], base_path: str):
     """
     Writes the given RDF graphs to the specified base path following the standard FDP structure.
     Each graph is written to a separate TTL file in the appropriate subfolder based on its resource type and parent resource.
-    
+
     Parameters:
         graphs (list): A list of RDF graphs to be written to disk.
         base_path (str): The base path where the graphs will be written.
     """
-    utils = graphutils()
-
+    
+    combined_graph = Graph()
     for graph in graphs:
         # Determine the resource type and parent resource from the graph
-        resource_id = utils.get_resource_id(graph)
-        resource_type = utils.get_resource_type(graph, resource_id)
-        parent_resource = utils.get_resource_parent(graph, resource_id)
+        resource_id = identify_main_resource(graph, "config/context.ttl")
+        resource_type = utils.get_resource_type(graph, resource_id).removeprefix("http://www.w3.org/ns/dcat#")
+        combined_graph += graph
+        parent_resource_uri = utils.get_resource_parent(graph, resource_id)
+        parent_resource = utils.get_title(combined_graph.cbd(parent_resource_uri))
 
         # Create the appropriate subfolder based on the resource type and parent resource
-        #FIXME now tries to attach an uri to the folder name, which is not allowed. Need to sanitize the uri to a valid folder name or obtain the title of the parent resource instead
         subfolder_path = os.path.join(base_path, f"{resource_type}_{parent_resource}")
         os.makedirs(subfolder_path, exist_ok=True)
         

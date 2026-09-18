3be7926e8ae59c3dbeb8d985e2c1cb59c7466313
KarolisCremers <karolis_cremers@live.nl>
2026-09-18 17:13:33 +0200

    Pep8 and fix connector imports

--- tests/systemttest/test_pipeline.py
+++ tests/systemttest/test_pipeline.py
@@ -1,20 +1,21 @@
 import sys
 from pathlib import Path
+
 import yaml
 
 # Ensure `src` is on sys.path so tests can import the package without installation
 sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
 
-from fdpmigrator.extract.TTL_extract_data import TTLExtractor
-from fdpmigrator.transform.construct_mapper import ConstructMapper
+from fdpmigrator.connector.ttl import TTLconnector
 from fdpmigrator.load.write_to_folder import write_to_folder
+from fdpmigrator.transform.construct_mapper import ConstructMapper
 
-query_targets = yaml.safe_load(open("config/query_targets.yaml"))["query_targets"]
-TTL_EXTRACTOR = TTLExtractor(base_path=Path("tests/data/fdp_files/FDP_LUMCFAIRDataPoint/Catalog_COMODULATECOVIDINFLUENZAcohortLUMC"), )
+query_targets = yaml.safe_load(open("config/query_targets_covid.yaml"))["query_targets"]
+TTL_connector = TTLconnector(base_path=Path("tests/data/fdp_files/FDP_LUMCFAIRDataPoint/Catalog_COMODULATECOVIDINFLUENZAcohortLUMC"), )
 
 mapped_graphs = []
 
-for graph in TTL_EXTRACTOR.generator():
+for graph in TTL_connector.generator():
     print(f"Found TTL file: {graph}")
     CONSTRUCT_MAPPER = ConstructMapper(graph=graph, query_targets=query_targets, query_folder=Path("SPARQL"))
     result_graph = CONSTRUCT_MAPPER.apply()
